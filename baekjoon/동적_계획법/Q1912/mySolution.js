b8c9370010ce8684ecf6aceb902f40945937e35f
Yujeong Choung <yujeong.choung@gmail.com>
2022-06-08 22:46:43 +0900

    [백준] 동적 계획법
    - 문제 'Q1912' mySolution 파일 수정

--- baekjoon/동적_계획법/Q1912/mySolution.js
+++ baekjoon/동적_계획법/Q1912/mySolution.js
@@ -1,21 +1,23 @@
-// https://www.acmicpc.net/problem/1904
+// https://www.acmicpc.net/problem/1912
 
 const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 
 (function mySolution(){
     let [n, inputArray] = fs.readFileSync(filePath).toString().trim().split("\n");
-    inputArray = inputArray.split(' ');
+    inputArray = inputArray.split(' ').map(num => Number(num));
 
     let dp = Array.from({length: Number(n)}, () => 0);
+    dp[0] = Number(inputArray[0]);
 
-    for(let i=0; i<dp.length; i++){
-        for(let j=1; i<=j; j++){
-
+    for(let i=1; i<dp.length; i++){
+        if(inputArray[i] > inputArray[i] + dp[i-1]){
+            dp[i] = inputArray[i];
         }
-
+        else {
+            dp[i] = inputArray[i] + dp[i - 1];
+        }
     }
 
-
-    console.log();
+    console.log(Math.max(...dp));
 })();
