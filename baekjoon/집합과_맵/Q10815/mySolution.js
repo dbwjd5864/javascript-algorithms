324386b1f6664e26bf8da4c1c03e94bfbc59fd71
Yujeong Choung <yujeong.choung@gmail.com>
2022-07-08 20:03:25 +0900

    [백준] 집합과 맵
    - 'Q10815' input 및 mySolution 추가

---
+++ baekjoon/집합과_맵/Q10815/mySolution.js
@@ -0,0 +1,28 @@
+// https://www.acmicpc.net/problem/10815
+
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+
+(function mySolution(){
+    const [cardLength, cardLists, ownCardLength, ownCardLists ] = fs.readFileSync(filePath).toString().trim().split("\n");
+    const cardListArr = cardLists.trim().split(" ");
+    const ownCardListArr = ownCardLists.trim().split(" ");
+
+    const answer = [];
+    const map = new Map();
+
+    cardListArr.map(card => {
+        map.set(card, 1 );
+    })
+
+    ownCardListArr.map(card => {
+        if(map.has(card)){
+            answer.push(1);
+        }
+        else{
+            answer.push(0);
+        }
+    })
+
+    console.log(answer.join(' '));
+}());
