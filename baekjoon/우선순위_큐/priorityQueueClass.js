216f32407e9a47117ad971c444e4c2ac9e3639b2
yujeong-choung <yujeong.choung@gmail.com>
2022-07-22 09:23:15 +0900

    [백준] 우선순위 큐
    - 우선순위 큐에 삽일한 데이터 및 우선순위 큐 클래스 생성 중

---
+++ baekjoon/우선순위_큐/priorityQueueClass.js
@@ -0,0 +1,30 @@
+// 우선순위 큐에 삽입되는 데이터 클래스
+export class PriorityQueueElement {
+    constructor(element, priority) {
+        this.element = element;
+        this.priority = priority;
+    }
+}
+
+// 우선순위 큐
+export class PriorityQueue{
+    constructor(){
+        this.queue = [];
+    }
+
+    enqueue(element, priority){
+
+    }
+
+    dequeue(){
+
+    }
+
+    getLength(){
+
+    }
+
+    print(){
+
+    }
+}
